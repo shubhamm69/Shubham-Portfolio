516a1ea373615322fd8a6c4f38a48f407b3320a4
Shubham Singh <shubham212a@gmail.com>
2023-08-02 22:26:28 +0530

    changes made to globals.css

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div>
+            <div>©2023 made with love ~Shubham Singh, All rights reserved</div>
+            <div> </div>
+        </div>
+    )
+}
+
+export default Footer
