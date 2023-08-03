132cc343b18b8e93000ee3ea74c258f760a9a329
Shubham Singh <shubham212a@gmail.com>
2023-08-03 19:38:18 +0530

    added module css file in each directory

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,9 @@
+import React from 'react'
+import styles from './page.module.css'
+
 function Contact() {
     return (
-        <div>
+        <div className={styles.container}>
             Contact
         </div>
     )
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,9 @@
+import React from 'react'
+import styles from './page.module.css'
+
 function Portfolio() {
     return (
-        <div>
+        <div className={styles.container}>
             Portfolio
         </div>
     )
