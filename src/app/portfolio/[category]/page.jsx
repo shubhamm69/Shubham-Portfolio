5401e770b27d28e62ef3aa0fb97ef3ef818949bc
Shubham Singh <shubham212a@gmail.com>
2023-08-04 21:24:53 +0530

    completed portfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/button/Button'
+import Image from 'next/image'
 
-function Category() {
+function Category({ params }) {
     return (
-        <div>
-            Category
+        <div className={styles.container}>
+            <h1 className={styles.catTitle}>{params.Category}</h1>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Button text="See more" url="#"/>
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image 
+                    className={styles.img}
+                    src="https://images.unsplash.com/photo-1690972909011-09fbbf8ab29c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80" alt="test" fill={true}/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 function Portfolio() {
     return (
         <div className={styles.container}>
-            Portfolio
+            <h1 className={styles.selectTitle}>Select a project</h1>
+            <div className={styles.items}>
+                <Link href="/portfolio/illustrations" className={styles.item} >
+                    <span className={styles.itemTitle}>Project 1</span> 
+                </Link>
+                <Link href="/portfolio/websites" className={styles.item} >
+                    <span className={styles.itemTitle}>Project 1</span>
+                </Link>
+                <Link href="/portfolio/apps" className={styles.item} >
+                    <span className={styles.itemTitle}>Project 1</span>
+                </Link>
+            </div>
         </div>
     )
 }
