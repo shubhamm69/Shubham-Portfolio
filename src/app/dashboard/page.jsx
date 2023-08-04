711aba6935f400bb519fe1f0d419239e42b9bf82
Shubham Singh <shubham212a@gmail.com>
2023-08-05 03:10:37 +0530

    added pnpm

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styles from './page.module.css'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 function Dashboard() {
     return (
