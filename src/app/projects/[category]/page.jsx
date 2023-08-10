ac3499deec20eaec28affc5f06a82a6a7a16fc20
Shubham Singh <shubham212a@gmail.com>
2023-08-10 14:11:50 +0530

    Making UI Responsive #3 updating Next version

--- src/app/projects/[category]/page.jsx
+++ src/app/projects/[category]/page.jsx
@@ -1,44 +1,38 @@
-import React from 'react'
-import styles from './page.module.css'
-import Button from '@/components/button/Button'
-import Image from 'next/image'
-import { items } from "./data.js"
-import { notFound } from 'next/navigation'
+import React from 'react';
+import styles from './page.module.css';
+import Button from '@/components/button/Button';
+import Image from 'next/image';
+import { items } from './data.js';
 
 const getData = (cat) => {
-    const data = items[cat]
-
-    if (data) {
-        return data
-    }
-    else {
-        return notFound()
-    }
-}
+    const data = items[cat];
+    return data || [];
+};
 
 function Category({ params }) {
-    const data = getData(params.category)
+    const data = getData(params.category);
     return (
         <div className={styles.container}>
             <h1 className={styles.catTitle}>{params.Category}</h1>
-            
-        {data.map((item) => (
-
-            <div className={styles.item} key={item.id}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>{item.title}</h1>
-                    <p className={styles.desc}>{item.desc}</p>
-                    <Button text="See more" url="#"/>
+            {data.map((item, index) => (
+                <div
+                    className={`${styles.item} ${index % 2 === 0 ? styles.oddItem : styles.evenItem}`}
+                    key={item.id}
+                >
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>{item.title}</h1>
+                        <p className={styles.desc}>{item.desc}</p>
+                        <div className={styles.buttonContainer}>
+                            <Button text="See more" url="#" />
+                        </div>
+                    </div>
+                    <div className={styles.imgContainer}>
+                        <Image className={styles.img} src={item.image} alt={item.title} layout="fill" objectFit="cover" />
+                    </div>
                 </div>
-                <div className={styles.imgContainer}>
-                    <Image 
-                    className={styles.img}
-                    src={item.image} alt="test" fill={true}/>
-                </div>
-            </div>
-        ))}
+            ))}
         </div>
-    )
+    );
 }
 
-export default Category
+export default Category;
